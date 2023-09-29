358e5e54df58fcdcb75fda050e28d1e9bc65ed3f
Artemis-Faul <elizabetbonnar@gmail.com>
2023-09-29 15:44:27 +0300

    First page

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// PRODUCTS SWIPER
+let swiperProd = new Swiper(".swiperProd", {
+  loop: true,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 1000,
+  },
+  speed: 1000,
+  slidesPerView: 3,
+});
+
+let swiperTrust = new Swiper(".swiperTrust", {
+  loop: true,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 1000,
+  },
+  speed: 1000,
+  slidesPerView: 3,
+});
+
+$(document).ready(function () {
+  var nav = $(".nav");
+  window.addEventListener("scroll", function () {
+    var top = $(this).scrollTop();
+    if (top > 0) {
+      nav.children(".nav-link").removeClass("white");
+      $(".number").removeClass("white");
+      $(".header ").css("background", "#fff");
+      $(".logo").attr("src", "images/main-page/logo.svg");
+      $(".menu-icons")
+        .children(".menu-icon")
+        .eq(0)
+        .attr("src", "images/main-page/icons/telegram.svg");
+      $(".menu-icons")
+        .children(".menu-icon")
+        .eq(1)
+        .attr("src", "images/main-page/icons/instagram.svg");
+      $(".menu-icons")
+        .children(".menu-icon")
+        .eq(2)
+        .attr("src", "images/main-page/icons/email.svg");
+    } else {
+      nav.children(".nav-link").addClass("white");
+      $(".number").addClass("white");
+      $(".logo").attr("src", "images/main-page/logo-white.svg");
+      $(".header ").css("background", "transparent");
+
+      $(".menu-icons")
+        .children(".menu-icon")
+        .eq(0)
+        .attr("src", "images/main-page/icons/instagram_white.svg");
+      $(".menu-icons")
+        .children(".menu-icon")
+        .eq(1)
+        .attr("src", "images/main-page/icons/telegram_white.svg");
+      $(".menu-icons")
+        .children(".menu-icon")
+        .eq(2)
+        .attr("src", "images/main-page/icons/email_white.svg");
+    }
+  });
+});
+
+// $(document).ready(function(){
+//   var w = $(window).width(); // Получаем ширину окна
+//   if (w <= 1601) {
+//     // Если ширина окна меньше, либо равна 600
+//     // $(".banner__img_one").css("animation-name", "updownone1600");
+//   } else if (w <= 1536) {
+//     // Если ширина окна меньше, либо равна 600
+//     // $(".banner__img_one").css("animation-name", "updownone1600");
+//   }
+// });
