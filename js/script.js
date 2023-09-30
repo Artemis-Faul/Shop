b8b9410a7c68d3e21eda5f0291a99d4231d42fd8
Artemis-Faul <elizabetbonnar@gmail.com>
2023-09-30 23:51:50 +0300

    Second page without infogarph

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
+$(document).ready(function () {
+  var main = $(".header_main");
+  var nav = $(".nav");
+  if (main) {
+    window.addEventListener("scroll", function () {
+      var top = $(this).scrollTop();
+      if (top > 0) {
+        nav.children(".nav-link").removeClass("white");
+        $(".number").removeClass("white");
+        $(".header ").css("background", "#fff");
+        $(".logo").attr("src", "images/main-page/logo.svg");
+        $(".menu-icons")
+          .children(".menu-icon")
+          .eq(0)
+          .attr("src", "images/main-page/icons/telegram.svg");
+        $(".menu-icons")
+          .children(".menu-icon")
+          .eq(1)
+          .attr("src", "images/main-page/icons/instagram.svg");
+        $(".menu-icons")
+          .children(".menu-icon")
+          .eq(2)
+          .attr("src", "images/main-page/icons/email.svg");
+      } else {
+        nav.children(".nav-link").addClass("white");
+        $(".number").addClass("white");
+        $(".logo").attr("src", "images/main-page/logo-white.svg");
+        $(".header ").css("background", "transparent");
+
+        $(".menu-icons")
+          .children(".menu-icon")
+          .eq(0)
+          .attr("src", "images/main-page/icons/instagram_white.svg");
+        $(".menu-icons")
+          .children(".menu-icon")
+          .eq(1)
+          .attr("src", "images/main-page/icons/telegram_white.svg");
+        $(".menu-icons")
+          .children(".menu-icon")
+          .eq(2)
+          .attr("src", "images/main-page/icons/email_white.svg");
+      }
+    });
+  }
+});
+
 // PRODUCTS SWIPER
 let swiperProd = new Swiper(".swiperProd", {
   loop: true,
@@ -9,6 +55,7 @@ let swiperProd = new Swiper(".swiperProd", {
   slidesPerView: 3,
 });
 
+// TRUST SWIPER
 let swiperTrust = new Swiper(".swiperTrust", {
   loop: true,
   spaceBetween: 24,
@@ -19,47 +66,15 @@ let swiperTrust = new Swiper(".swiperTrust", {
   slidesPerView: 3,
 });
 
-$(document).ready(function () {
-  var nav = $(".nav");
-  window.addEventListener("scroll", function () {
-    var top = $(this).scrollTop();
-    if (top > 0) {
-      nav.children(".nav-link").removeClass("white");
-      $(".number").removeClass("white");
-      $(".header ").css("background", "#fff");
-      $(".logo").attr("src", "images/main-page/logo.svg");
-      $(".menu-icons")
-        .children(".menu-icon")
-        .eq(0)
-        .attr("src", "images/main-page/icons/telegram.svg");
-      $(".menu-icons")
-        .children(".menu-icon")
-        .eq(1)
-        .attr("src", "images/main-page/icons/instagram.svg");
-      $(".menu-icons")
-        .children(".menu-icon")
-        .eq(2)
-        .attr("src", "images/main-page/icons/email.svg");
-    } else {
-      nav.children(".nav-link").addClass("white");
-      $(".number").addClass("white");
-      $(".logo").attr("src", "images/main-page/logo-white.svg");
-      $(".header ").css("background", "transparent");
-
-      $(".menu-icons")
-        .children(".menu-icon")
-        .eq(0)
-        .attr("src", "images/main-page/icons/instagram_white.svg");
-      $(".menu-icons")
-        .children(".menu-icon")
-        .eq(1)
-        .attr("src", "images/main-page/icons/telegram_white.svg");
-      $(".menu-icons")
-        .children(".menu-icon")
-        .eq(2)
-        .attr("src", "images/main-page/icons/email_white.svg");
-    }
-  });
+// TRUST SWIPER
+let swiperTeam = new Swiper(".swiperTeam", {
+  loop: true,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 1000,
+  },
+  speed: 1000,
+  slidesPerView: 4,
 });
 
 // $(document).ready(function(){
